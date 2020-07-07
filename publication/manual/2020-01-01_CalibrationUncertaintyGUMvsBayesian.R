abf76e057cb03bcca89699c95fe5ff978934f72d
smartens83 <steffen.martens@tu-berlin.de>
2020-07-07 10:47:21 +0200

    -m update talks

---
+++ publication/manual/2020-01-01_CalibrationUncertaintyGUMvsBayesian.R
@@ -0,0 +1,35 @@
+---
+abstract: 
+abstract_short:
+authors: 
+- admin
+- Katy Klauenberg
+- Clemens Elster
+date: "2020-01-01"
+doi: "http://doi.org/10.5281/zenodo.3858121"
+publication: "Zenodo EMUE repository"
+publication_short: "Zenodo"
+publication_types: 
+- "4"
+title: "EMUE-D6-2-CalibrationUncertaintyGUMvsBayesian"
+featured: false
+image:
+  caption: ""
+focal_point: ""
+preview_only: false
+projects: ["UncerEval"]
+tags:
+- regression
+- Bayes
+- uncertainty evaluation
+url_preprint: ""
+url_code: ""
+url_dataset: ""
+url_pdf: ""
+url_poster: ""
+url_project: ""
+url_slides: ""
+url_source: ""
+url_video : ""
+---
+  
